8f5c6283c81cc28b5847184b998e1e03f7db8077
MaksFuji <70910855+MaksFuji@users.noreply.github.com>
2023-04-22 20:20:55 +0300

    #25_loop_error_fix
    added comments, removed directory search function

--- #11/main.py
+++ #11/main.py
@@ -2,7 +2,7 @@ import os
 import time
 
 from compress import run_coder, run_decoder
-from utils import load_image, search_dir, save_img, get_rescaled_cv2, \
+from utils import load_image, save_img, get_rescaled_cv2, \
     metrics_img, write_metrics_in_file, create_data_dir, create_dir
 from common.logging_sd import configure_logger
 import cv2
@@ -11,6 +11,8 @@ logger = configure_logger(__name__)
 
 size = (512, 512)
 
+dir_path = "data/input"
+dir_name = "input"
 output_rescaled_data_path = "rescaled"
 test_path = "test"
 
@@ -23,43 +25,45 @@ def default_main(is_quantize=True, is_save=False, save_metrics=True, save_rescal
     if not os.path.exists(f"data/output"):
         create_data_dir()
 
-    for dir_path, dir_name in search_dir():
-        try:
-            count = 0
-            logger.debug(f"get files in dir = {dir_name}")
-            for img_path, img_name in load_image(dir_path):
-                if img_name.__contains__("DS_Store"):
-                    continue
-                logger.debug(f"{img_path}, {img_name}")
-                count += 1
-                print(count)
-                logger.debug(f"compressing file {img_name} in dir {dir_name}; count = {count};" 
-                             f" img size = {size} max 9")
+    try:
+        count = 0
+        logger.debug(f"get files in dir = {dir_name}")
+        
+        # цикл обработки кадров
+        for img_path, img_name in load_image(dir_path):
+            count += 1
+            logger.debug(f"compressing file {img_name} in dir {dir_name}; count = {count};" 
+                            f" img size = {size} max 9")
 
-                if not os.path.exists(f"data/output/{count}_run"):
-                    create_dir(f"{count}_run")
-                save_dir_name = f"{count}_run"
+            # создание директории для сохранения сжатого изображения и резултатов метрик
+            if not os.path.exists(f"data/output/{count}_run"):
+                create_dir(f"{count}_run")
+            save_dir_name = f"{count}_run"
 
-                image = cv2.imread(img_path)
+            # считывание кадра из директории input
+            image = cv2.imread(img_path)
 
-                img = get_rescaled_cv2(image, size)
-                if save_rescaled_out:
-                    save_img(img, path=save_dir_name, name_img=img_name)
+            # сжатие кадра для отправления на НС
+            img = get_rescaled_cv2(image, size)
+            if save_rescaled_out:
+                save_img(img, path=save_dir_name, name_img=img_name)
 
-                run_coder()
-                run_decoder()
+            # функции НС
+            run_coder()
+            run_decoder()
 
-                if save_metrics:   
-                    width = int(image.shape[1])
-                    height = int(image.shape[0])
-                    dim = (width, height)
-                    rescaled_img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA) 
-                    data = metrics_img(image, rescaled_img)
-                    write_metrics_in_file(f"data/output/{save_dir_name}", data, img_name)
+            # сохранение метрик
+            if save_metrics:   
+                width = int(image.shape[1])
+                height = int(image.shape[0])
+                dim = (width, height)
+                rescaled_img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA) 
+                data = metrics_img(image, rescaled_img)
+                write_metrics_in_file(f"data/output/{save_dir_name}", data, img_name)
 
-        except Exception as err:
-            logger.error(f"error is file {img_name} "
-                         f"for write in dir {save_dir_name}\n error {err}")
+    except Exception as err:
+        logger.error(f"error is file {img_name} "
+                        f"for write in dir {save_dir_name}\n error {err}")
     end = time.time() - start  ## собственно время работы программы
     logger.info(f'Complete: {end}')
 
