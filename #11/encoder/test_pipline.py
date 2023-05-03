62f206a6861b00467d9f9561cd592ba919366bb6
MaksFuji <70910855+MaksFuji@users.noreply.github.com>
2023-05-03 21:10:28 +0300

    #32_model_train_test

--- #11/encoder/test_pipline.py
+++ #11/encoder/test_pipline.py
@@ -1,123 +1,47 @@
 import os
 import time
 
-
-import os
-import time
-
 import cv2
 import numpy as np
-from PIL import Image
 from numpy import ndarray
-import logging
+import torch
+from PIL import Image
+from torchvision.transforms import Compose, ToPILImage, ToTensor, Resize
+
+from model import VAE
 
 
-SCALED_SIZE_DEFAULT = (1200, 1200)
-INPUT_DATA_PATH = "data/test"
-OUTPUT_DATA_PATH = "../data/train/output"
-
-SIZE = (512, 512)
-
-DIR_PATH_INPUT = "../data/train/input"
-DIR_PATH_OUTPUT = "../data/train/output"
-DIR_NAME = "input"
-TEST_PATH = "test"
+WEIGHTS_PATH = "../weights/encoder_weights.pth"
 
 
-def run_coder():
-    ...
+# Создание модели и загрузка сохраненных весов
+model = VAE()
+model.load_state_dict(torch.load(WEIGHTS_PATH))
+model.eval()
 
 
-def run_decoder():
-    ...
+# Определение трансформаций
+transforms = Compose([
+    Resize((512, 512)),
+    ToTensor()
+])
+
+# Загрузка изображения и применение трансформаций
+input_image = Image.open(f"{DIR_PATH_OUTPUT}/0_image.jpg")
+input_tensor = transforms(input_image).unsqueeze(0)
+
+# Прогнозирование выходного изображения
+with torch.no_grad():
+    output_tensor = model(input_tensor)
+
+# Преобразование тензора в изображение
+output_image = ToPILImage()(output_tensor.squeeze())
+
+# Сохранение выходного изображения
+output_image.save("output_image.png")
 
 
 
 
-def create_dir(new_dir_name: str, index: str = ""):
-        os.makedirs(f"{OUTPUT_DATA_PATH}/{new_dir_name}/")
 
 
-def load_image(path: str = INPUT_DATA_PATH): 
-        for filename in os.listdir(path):
-            if filename.endswith('.png') or filename.endswith('.jpg'):
-                f = os.path.join(path, filename)
-                if os.path.isfile(f):
-                    yield f, filename
-
-
-def save_img(img, path: str, name_img: str = 'default'):
-    if os.path.exists(f'{OUTPUT_DATA_PATH}/{path}'):
-            cv2.imwrite(f'{OUTPUT_DATA_PATH}/{path}/0_{name_img}', img)
-
-
-def get_rescaled_cv2(image, scaled_size: tuple = (512, 512)):
-        res = cv2.resize(image, scaled_size)
-        return res
-
-
-def write_metrics_in_file(path: str, data: tuple, image_name: str, time: time):
-        data_str = f"image_name: {image_name}\n" \
-                   f"ssim_data = {data[0]}\n" \
-                   f"pirson_data = {data[1]}\n" \
-                   f"cosine_similarity = {data[2]}\n" \
-                   f"mse = {data[3]}\n" \
-                   f"hamming_distance = {data[4]}\n"\
-                   f"frame_compression_time = {time}\n"
-        with open(f"{path}/metrics.txt", mode='w') as f:
-            f.write(data_str)
-
-
-def metrics_img(image, denoised_img) -> tuple:
-        img1 = (np.array(image).ravel())
-        img2 = (np.array(denoised_img).ravel())
-        ssim_data = metrics.ssim(image, denoised_img)
-        pirson_data = metrics.cor_pirson(img1, img2)
-        cosine_similarity = metrics.cosine_similarity_metric(img1, img2)
-        mse = metrics.mse_metric(image, denoised_img)
-        hamming_distance = metrics.hamming_distance_metric(image, denoised_img)
-        result_metrics: tuple = (ssim_data, pirson_data, cosine_similarity, mse, hamming_distance)
-        return result_metrics
-
-def default_main(is_quantize=True, is_save=False, save_metrics=True, save_rescaled_out=False, debug=False):
-    start = time.time()  ## точка отсчета времени
-
-    if not os.path.exists(DIR_PATH_INPUT):
-        os.makedirs(DIR_PATH_INPUT)
-    if not os.path.exists(DIR_PATH_OUTPUT):
-        os.makedirs(DIR_PATH_OUTPUT)
-
-    count = 0
-
-    # цикл обработки кадров
-    for img_path, img_name in load_image(DIR_PATH_INPUT):
-        start = time.time()
-
-        # считывание кадра из input
-        image = cv2.imread(img_path)
-        count += 1
-
-        # создание директории для сохранения сжатого изображения и резултатов метрик
-        if not os.path.exists(f"data/output/{count}_run"):
-            create_dir(f"{count}_run")
-        save_dir_name = f"{count}_run"
-
-        # сжатие кадра для отправления на НС
-        img = get_rescaled_cv2(image, SIZE)
-        if save_rescaled_out:
-            save_img(img, path=save_dir_name, name_img=img_name)
-
-        # функции НС
-        run_coder()
-        run_decoder()
-
-        end_time = time.time() - start
-
-        if is_save:
-            save_img(img, path=save_dir_name, name_img=img_name)
-
-
-    end = time.time() - start  ## собственно время работы программы
-
-if __name__ == '__main__':
-    default_main(is_quantize=True, is_save=True, save_metrics=True, save_rescaled_out=False)
--- #11/encoder/train_vae.py
+++ #11/encoder/train_vae.py
@@ -1,25 +1,80 @@
-import model
+"""
+Import necessary libraries to create a variational autoencoder
+The code is mainly developed using the PyTorch library
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+from PIL import Image
+from model import VAE
+from torchvision.transforms import Compose, ToPILImage, ToTensor, Resize
+
+
+"""
+Determine if any GPUs are available
+"""
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+"""
+Initialize Hyperparameters
+"""
+BATCH_SIZE = 128
+LEARNING_RATE = 1e-3
+NUM_EPOCHS = 100
+DIR_PATH_INPUT = "../data/train/input"
+DIR_PATH_OUTPUT = "../data/train/output"
+
+
+
+WEIGHTS_PATH = "../weights/encoder_weights.pth"
+
+
+"""
+Create dataloaders to feed data into the neural network
+Default MNIST dataset is used and standard train/test split is performed
+"""
+
+# Создание наборов данных и загрузчиков
+train_transforms = Compose([
+    Resize((512, 512)),
+    ToTensor()
+])
+
+
+train_dataset = MyDataset(DIR_PATH_INPUT, DIR_PATH_OUTPUT, transform=train_transforms)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+"""
+Initialize the network and the Adam optimizer
+"""
+net = VAE().to(device)
+optimizer = torch.optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
 
 """
 Training the network for a given number of epochs
 The loss after every epoch is printed
 """
-for epoch in range(model.num_epochs):
-    for idx, data in enumerate(model.train_loader, 0):
+for epoch in range(NUM_EPOCHS):
+    for idx, data in enumerate(train_loader, 0):
         imgs, _ = data
-        imgs = imgs.to(model.device)
+        imgs = imgs.to(device)
 
         # Feeding a batch of images into the network to obtain the output image, mu, and logVar
-        out, mu, logVar = model.net(imgs)
+        out, mu, logVar = net(imgs)
 
         # The loss is the BCE loss combined with the KL divergence to ensure the distribution is learnt
-        kl_divergence = -0.5 * model.torch.sum(1 + logVar - mu.pow(2) - logVar.exp())
+        kl_divergence = -0.5 * torch.sum(1 + logVar - mu.pow(2) - logVar.exp())
         loss = F.binary_cross_entropy(out, imgs, size_average=False) + kl_divergence
 
         # Backpropagation based on the loss
-        model.optimizer.zero_grad()
+        optimizer.zero_grad()
         loss.backward()
-        model.optimizer.step()
+        optimizer.step()
 
     print('Epoch {}: Loss {}'.format(epoch, loss))
+torch.save(net.state_dict(), WEIGHTS_PATH)
 
