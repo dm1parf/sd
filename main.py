0f6c0bcef962dd30e7454ec623ccc1d8c773e821
MaksFuji <70910855+MaksFuji@users.noreply.github.com>
2023-07-19 18:51:20 +0300

    62_video
    Реализовано использование видеофайла для подачи на пайплайн. Метрика Vmaf была убрана, так как не удалось найти реализацию через массивы, а не через пути в сохраненным изображениям.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import time
 import numpy as np
 
 from compress import run_coder, run_decoder
-from constants.constant import DIR_NAME, DIR_PATH_INPUT, DIR_PATH_OUTPUT, SIZE
-from utils import load_image, save_img, get_rescaled_cv2, metrics_img, write_metrics_in_file, create_dir
+from constants.constant import DIR_NAME, DIR_PATH_INPUT, DIR_PATH_OUTPUT, SIZE, USE_VIDEO, SHOW_VIDEO
+from utils import load_image, save_img, get_rescaled_cv2, metrics_img, write_metrics_in_file, create_dir,load_frame_video
 from common.logging_sd import configure_logger
 import cv2
 
@@ -24,21 +24,34 @@ def default_main(is_quantize=True, is_save=False, save_metrics=True, save_rescal
     count = 0
     logger.debug(f"get files in dir = {DIR_NAME}")
 
+    if SHOW_VIDEO:
+        window_name = 'Video'
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+    load_frame = load_image if not USE_VIDEO else load_frame_video
+
     for dir_name in os.listdir(DIR_PATH_INPUT):   # цикл обработки кадров
-        for img_path, img_name in load_image(f"{DIR_PATH_INPUT}/{dir_name}"):
+        # for img_path, img_name in load_frame(f"{DIR_PATH_INPUT}/{dir_name}"):
+        for params_frame in load_frame(f"{DIR_PATH_INPUT}/{dir_name}"):
             start = time.time()
 
+            if not USE_VIDEO:
+                img_path, img_name = params_frame
+            else:
+                frame, img_name, video_name = params_frame
+
             # считывание кадра из input
-            image = cv2.imread(img_path)
+            if USE_VIDEO:
+                image = frame
+            else:
+                image = cv2.imread(img_path)
             count += 1
             logger.debug(f"compressing file {img_name} in dir {DIR_NAME}; count = {count};"
                          f" img size = {SIZE} max 9")
 
-
             if not os.path.exists(f"{DIR_PATH_OUTPUT}/{dir_name}_run"):
                 create_dir(DIR_PATH_OUTPUT, f"{dir_name}_run")
             save_parent_dir_name = f"{dir_name}_run"
-
 
             # создание директории для сохранения сжатого изображения и резултатов метрик
             if not os.path.exists(f"{DIR_PATH_OUTPUT}/{save_parent_dir_name}/{count}_run"):
@@ -55,6 +68,10 @@ def default_main(is_quantize=True, is_save=False, save_metrics=True, save_rescal
             uncompress_img = run_decoder(compress_img)
             uncompress_img = cv2.cvtColor(np.array(uncompress_img), cv2.COLOR_RGB2BGR)
 
+            if SHOW_VIDEO:
+                cv2.imshow(window_name, uncompress_img)
+                cv2.waitKey(25)
+
             end_time = time.time() - start
 
             if is_save:
@@ -66,11 +83,12 @@ def default_main(is_quantize=True, is_save=False, save_metrics=True, save_rescal
                 height = int(image.shape[0])
                 dim = (width, height)
                 rescaled_img = cv2.resize(uncompress_img, dim, interpolation=cv2.INTER_AREA)
-                data = metrics_img(image, rescaled_img, img_path, f"{DIR_PATH_OUTPUT}/{save_parent_dir_name}/{save_dir_name}/0_{img_name}")
+                data = metrics_img(image, rescaled_img)
                 write_metrics_in_file(f"{DIR_PATH_OUTPUT}/{save_parent_dir_name}/{save_dir_name}", data, img_name, end_time)
 
         end = time.time() - start  ## собственно время работы программы
         logger.debug(f'Complete: {end}')
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
